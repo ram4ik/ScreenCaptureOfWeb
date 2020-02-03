94cc7df6d0855b47da6ec0ab61632c9a09d8a155
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-03 12:38:36 +0200

    Initial Commit

---
+++ ScreenCaptureOfWeb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScreenCaptureOfWeb
+//
+//  Created by ramil on 03.02.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
