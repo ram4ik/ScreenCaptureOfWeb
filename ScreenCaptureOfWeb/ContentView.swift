2b1c23168a3c389e16cd80d0a5b42e31c1720399
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-03 12:57:14 +0200

    init

--- ScreenCaptureOfWeb/ContentView.swift
+++ ScreenCaptureOfWeb/ContentView.swift
@@ -7,10 +7,73 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    
+    @State private var urlString = "https://www.apple.com"
+    @State private var uiImage: UIImage? = nil
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            WebView(url: URL(string: urlString)!)
+            SheetView(uiImage: $uiImage)
+        }
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    
+    var url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
+
+struct SheetView: View {
+    @State private var showTrigger = false
+    
+    @Binding var uiImage: UIImage?
+    
+    var body: some View {
+        VStack {
+            Button("Check Image") {
+                self.showTrigger = true
+            }
+        }.sheet(isPresented: $showTrigger, onDismiss: {
+            print("Show Screenshot")
+        }) {
+            ScreenShot(uiImage: self.$uiImage)
+        }
+    }
+}
+
+struct ScreenShot: View {
+    @Environment(\.presentationMode) var presentation
+    
+    @Binding var uiImage: UIImage?
+    
+    var body: some View {
+        VStack {
+            
+            if uiImage != nil {
+                VStack {
+                    Image(uiImage: self.uiImage!)
+                }.background(Color.green)
+                    .frame(width: 500, height: 700)
+            }
+            Spacer()
+            Button("Close") {
+                self.presentation.wrappedValue.dismiss()
+            }
+            Spacer()
+        }.font(.custom("SFProText-Bold", size: 25))
     }
 }
 
